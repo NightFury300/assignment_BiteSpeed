d91de4c06f155123dd8dd5df190fe7bb91a230eb
Shubh Saxena <shubhsaxena447@gmail.com>
2025-02-09 20:37:48 +0530

    feat: Added linkContacts controller for the API Endpoint

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(json());
 
 import userRouter from './routes/userRouter.routes.js';
+import { Contact } from './models/contact.model.js';
 
 app.use("/",userRouter);
+//console.log(await Contact.findAll({}));
 
 export { app };
